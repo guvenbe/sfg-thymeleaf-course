77199cabcb08aa237eab0a5571ffc30e3e60775b
Bora Guven <guvenbe@gmail.com>
2019-05-16 23:23:54 -0400

    conditionals

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -1,13 +1,33 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class ProductController {
 
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
     @RequestMapping("/product")
     public String getProduct(){
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProductById(@PathVariable Integer id, Model model){
+        System.out.println("id request=" + id);
+        model.addAttribute("product",productService.getProduct(id));
         return "product";
     }
 }
